5afde6bd8770bfb45bd1c4400a90d07f8f765d63
O'vusi Nobert Jakpor <60452919+Ovusi@users.noreply.github.com>
2022-07-18 20:58:36 +0100

    Update HavenToken.sol

--- HavenToken.sol
+++ HavenToken.sol
@@ -5,14 +5,67 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HavenToken is ERC20 {
-    uint256 private stakeAllocation = 10000000;
-    uint256 private maxSupply = 100000000;
-    uint256 private initialMintSupply = 90000000;
+    uint256 private rewardSupply = 100000000 * 10**9;
+    uint256 private initialMintSupply = 90000000 * 10**decimals();
+    uint256 totalRewarded;
+
+    address marketplace = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address stakingAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address owner;
 
     string private _name = "Haven Token";
     string private _symbol = "HVX";
 
+    /*///////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
+
     constructor() ERC20(_name, _symbol) {
-        _mint(msg.sender, initialMintSupply * 10**decimals());
+        owner = msg.sender;
+        _mint(msg.sender, initialMintSupply);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Logic
+    //////////////////////////////////////////////////////////////*/
+
+    function marketplaceRewards(address account, uint256 amount) external {
+        uint256 floatReward = (amount * 10) ^ 0;
+
+        require(msg.sender == marketplace || msg.sender == stakingAddress);
+        require(totalRewarded < rewardSupply);
+        require(floatReward < rewardSupply);
+
+        _mint(account, amount);
+
+        totalRewarded += floatReward;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Setter functions
+    //////////////////////////////////////////////////////////////*/
+
+    function setMarketAddress(address marketAddress) external {
+        require(msg.sender == owner);
+
+        marketplace = marketAddress;
+    }
+
+    function setStakingAddress(address stakingContractAddress) external {
+        require(msg.sender == owner);
+
+        stakingAddress = stakingContractAddress;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Getter functions
+    //////////////////////////////////////////////////////////////*/
+
+    function marketContract() external view returns (address) {
+        return marketplace;
+    }
+
+    function stakingContract() external view returns (address) {
+        return stakingAddress;
     }
 }
